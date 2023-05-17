2a1eb42d3656ca3798df232f5197edc5b5e91f1d
esmailsameh <132748715+esmailsameh@users.noreply.github.com>
2023-05-17 16:09:11 +0300

    Add files via upload

---
+++ task 8/Task 8.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[74]:
+
+
+import matplotlib.pyplot as plt
+
+path = r"S:\AOU\ML course\w6\smoke_detection_iot.csv"
+data = pd.read_csv(path)
+
+Yes_Fire = data[data['Fire_Alarm'] == 1]
+No_Fire = data[data['Fire_Alarm'] == 0]
+
+
+num = [len(Yes_Fire), len(No_Fire)]
+
+plt.pie(num,autopct='%1.1f%%',startangle=192,wedgeprops={'edgecolor' :"black"})
+plt.legend(['Yes_Fire','No_Fire'],loc='center right', bbox_to_anchor=(1, 0.5, 0.5, 0.5))
+plt.title('fire alatm gragh')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
